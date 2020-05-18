2360d2fc2894e8804783fea1226f5b45829f405c
Juan P <juan.p.32a@gmail.com>
2020-05-18 18:26:39 -0500

    uploated

--- comand.php
+++ comand.php
@@ -2,7 +2,7 @@
 <?php
 // imprime el nombre de usuario que tiene control del proceso php/httpd activo
 // (en un sistema con el ejecutable "whoami" disponible en la ruta)
-echo exec('pipenv shell');
+//echo exec('pipenv shell');
 echo exec('cd myrestapi');
 echo exec('manage.py runserver');
 ?>
